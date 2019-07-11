bd0178811cb9b1b42a94d0a05369a0144467f6ae
qaisertariq98 <49521081+qaisertariq98@users.noreply.github.com>
2019-07-11 17:26:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// WhatsApp-HomeWork
+
+let whatsApp = [
+    {
+        Name: "khadija-tul-Kubra",
+        date: "11-9-2019",
+        time: "3:00 p.m",
+        message: "Where???"
+    },
+
+    {
+        Name: "Kukab-Shakil",
+        date: "11-9-2019",
+        time: "9:45 p.m",
+        message: "have You done your work???"
+    },
+]
+// TechKaro-HomeWork
+let techKaro = {
+    name: "techkaro",
+    timePeriod: "8 months",
+    details: "web Development course" 
+students: [
+        {
+            Name: "Abdullah Rasheed",
+            education: "graduate",
+            age: "21",
+            height: "5'",
+            cnic: "42101-22890-1",
+            couresPeriod: "8months",
+
+
+
+        },
+        {
+            Name: "Ayeshakhan",
+            education: "graduate",
+            age: "21",
+            height: "5'",
+            cnic: "42101-22890-1",
+            couresPeriod: "8months",
+
+
+
+        }
+    ]
+}
